edac3f3c0155f84d1d0a1bcbea75ddfa9ffa3ff7
annichai <annichai@berkeley.edu>
2022-01-09 15:45:27 -0800

    underlines text

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 function reddenPage() {
-    document.body.style.backgroundColor = 'red';
+    var links = document.links;
+    for(var i = 0; i < links.length; i++) {
+        links[i].style.textDecoration = "underline";
+      }
   }
   
   chrome.action.onClicked.addListener((tab) => {
